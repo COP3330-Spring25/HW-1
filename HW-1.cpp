d939023c8244c787bed03f3859f039a8db41f876
CarlosTheran-FAMU <159965565+CarlosTheran-FAMU@users.noreply.github.com>
2024-12-23 14:00:14 -0500

    Carlos Theran

---
+++ HW-1.cpp
@@ -0,0 +1,19 @@
+//Please use this file to complete your assignmet
+
+#include <iostream>
+//include the needes libraries
+
+
+using namespace std;
+
+
+int sum(/*add the needed parameters*/)
+{
+
+    //complete the function
+}
+
+int main()
+{
+    //complete the code following the instructions
+}
